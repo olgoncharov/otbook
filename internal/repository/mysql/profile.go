627a2528b9ca7097058ac085cb0336b3e1bf27aa
Oleg Goncharov <oleg.gnch@gmail.com>
2022-09-10 22:33:47 +0300

    added profile ordering

--- internal/repository/mysql/profile.go
+++ internal/repository/mysql/profile.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f73927135a04f172bc2cdf01859613e7354d7efb94e92f6d7dabb824b2fa467
-size 5571
+oid sha256:67113d339d4d3ef9b6066decf91788cef2a59afb21948e1c551eede23d3701af
+size 5587
