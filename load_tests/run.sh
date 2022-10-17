b495ea7bfcc67eca230f19deede083f24ba0331f
Oleg Goncharov <oleg.gnch@gmail.com>
2022-10-18 01:03:22 +0300

    added load tests

--- load_tests/run.sh
+++ load_tests/run.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 docker run \
-    -v $(pwd):/var/loadtest \
+    -v $(pwd)/$1:/var/loadtest \
+    -v $(pwd)/overload_token.txt:/var/loadtest/overload_token.txt \
     -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent \
     --net host \
-    -it direvius/yandex-tank
+    -it olgoncharov/ytank
