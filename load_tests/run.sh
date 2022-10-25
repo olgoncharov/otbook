576892f9e79e519926f3fdd1a50b4400ad0552e7
Oleg Goncharov <oleg.gnch@gmail.com>
2022-10-26 02:52:06 +0300

    load tests for creating friends

--- load_tests/run.sh
+++ load_tests/run.sh
@@ -3,6 +3,7 @@
 docker run \
     -v $(pwd)/$1:/var/loadtest \
     -v $(pwd)/overload_token.txt:/var/loadtest/overload_token.txt \
+    -v $(pwd)/bin/pandora:/usr/bin/pandora \
     -v $SSH_AUTH_SOCK:/ssh-agent -e SSH_AUTH_SOCK=/ssh-agent \
     --net host \
     -it olgoncharov/ytank
